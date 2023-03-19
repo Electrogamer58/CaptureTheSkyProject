a55d4f0d4817d6839ac389b0e96ded8fea973d97
aregaIado <78439483+aregaIado@users.noreply.github.com>
2023-03-19 15:05:33 -0500

    Timer/clock hand movement
    - stopped updating slider instead rotating clock hand

--- Assets/Scripts/TimerCountdown.cs
+++ Assets/Scripts/TimerCountdown.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering.PostProcessing;
 public class TimerCountdown : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private RectTransform _clockHandParent;
     [SerializeField] public CircleSlider _circleSlider;
     [SerializeField] public PostProcessVolume _postProcessVolume;
     public bool _gameStart;
@@ -32,7 +33,22 @@ public class TimerCountdown : MonoBehaviour
             float timeToSubtract = 1.0f / _roundTime;
             val -= timeElapsed * timeToSubtract;
             val = Mathf.Clamp(val, 0.0f, 1.0f);
-            _circleSlider.UpdateSlider(val);
+            //_circleSlider.UpdateSlider(val);
+
+            //convert float to degrees
+            float deg = val * 360;
+            deg = Mathf.FloorToInt(deg % 360);
+
+            //only update every second (should be deg % 6 if 60 second timer)
+            if(deg % 3 == 0)
+            {
+                //_circleSlider.UpdateSlider(deg / 360);
+
+                Quaternion rot = new Quaternion();
+                rot.eulerAngles = new Vector3(_clockHandParent.rotation.eulerAngles.x, _clockHandParent.rotation.eulerAngles.y, deg);
+                _clockHandParent.SetPositionAndRotation(_clockHandParent.transform.position, rot);
+            }
+
         }
         if (val <= 0.3f)
         {
